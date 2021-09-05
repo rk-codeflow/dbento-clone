19fc706f17e7d5bb7bd442b78102daeafb4041b0
KEERAN <rjkeeran@gmail.com>
2021-09-05 14:24:43 +0545

    repo created

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// Slider js
+$(document).ready(function(){
+  $('.banner__sect').slick({
+    infinite: true,
+    arrows: true,
+    prevArrow: '<img class="arrow arrow-prev" src="/img/chevron-left.svg">',
+    nextArrow: '<img class="arrow arrow-next" src="/img/chevron-right.svg">'
+  });
+});
+
+// Feather icons
+  feather.replace();
